e97afa58019b68dd76361c60f2593974b01dbe1e
leonid-legit <leonid@legitsecurity.com>
2025-07-09 13:17:12 +0300

    add book route

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Import routes
 const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
+
+// Mount routes
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/books', bookRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
